5b9259381ef47aebbf146a328e1162b140a7f0ad
SoloDarkNet <solomonzabath@gmail.com>
2025-09-15 13:28:03 +0000

    Shopping Trends

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  //   TODO: Add your code for remove all cart items, increment cart item quantity, decrement cart item quantity, remove cart item
+
+  removeAllCartItem = () => {
+    this.setState({cartList: []})
+  }
+
+  incrementCartItemQuanity = id => {
+    this.setState(prevState => ({
+      cartList: prevState.cartList.map(eachCartItem => {
+        if (id === eachCartItem.id) {
+          const updatedQuanity = eachCartItem.quanity + 1
+          return {...eachCartItem, quanity: updatedQuanity}
+        }
+        return eachCartItem
+      }),
+    }))
+  }
+
+  decrementCartItemQuanity = id => {
+    const {cartList} = this.state
+    const productObject = cartList.find(eachCartItem => eachCartItem.id === id)
+    if (productObject.quanity > 1) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (id === eachCartItem.id) {
+            const updatedQuanity = eachCartItem.quanity - 1
+            return {...eachCartItem, quanity: updatedQuanity}
+          }
+          return eachCartItem
+        }),
+      }))
+    } else {
+      this.removeCartItem(id)
+    }
+  }
+
+  removeCartItem = id => {
+    const {cartList} = this.state
+    const updatedCartList = cartList.filter(
+      eachCartItem => eachCartItem.id === id,
+    )
+    this.setState({cartList: updatedCartList})
+  }
+
+  addCartItem = product => {
+    const {cartList} = this.state
+    const productObject = cartList.find(
+      eachCartItem => eachCartItem.id === product.id,
+    )
+
+    if (productObject) {
+      this.setState(prevState => ({
+        cartList: prevState.cartList.map(eachCartItem => {
+          if (eachCartItem.id === productObject.id) {
+            const updatedQuanity = eachCartItem.quanity + product.quanity
+            return {...eachCartItem, quanity: updatedQuanity}
+          }
+          return eachCartItem
+        }),
+      }))
+    } else {
+      const updatedCartList = [...cartList, product]
+
+      this.setState({cartList: updatedCartList})
+    }
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <CartContext.Provider
+        value={{
+          cartList,
+          addCartItem: this.addCartItem,
+          removeCartItem: this.removeCartItem,
+        }}
+      >
+        <Switch>
+          <Route exact path="/login" component={LoginForm} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path="/products" component={Products} />
+          <ProtectedRoute
+            exact
+            path="/products/:id"
+            component={ProductItemDetails}
+          />
+          <ProtectedRoute exact path="/cart" component={Cart} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </CartContext.Provider>
+    )
+  }
+}
+
+export default App
---
+++ src/components/CartSummary/index.js
@@ -0,0 +1,37 @@
+// Write your code here
+import CartContext from '../../context/CartContext'
+import './index.css'
+
+const CartSummary = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      let total = 0
+      cartList.forEach(eachCartItem => {
+        total += eachCartItem.price * eachCartItem.quanity
+      })
+
+      return (
+        <>
+          <div className="cart-summary-container">
+            <div className="order-total-view">
+              <h1>
+                <span className="order-total-label">Order Total:</span> Rs{' '}
+                {total}
+                {''}/-
+              </h1>
+              <p>{cartList.length} Items in cart</p>
+              <button className="Checkout-btn d-sm-none" type="button">
+                Checkout
+              </button>
+            </div>
+            <button className="Checkout-btn d-lg-none" type="button">
+              Checkout
+            </button>
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default CartSummary
